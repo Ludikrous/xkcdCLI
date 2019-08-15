28ef7c1794e49b4a70ab39cd1c987ce26f29ca4f
Dhanvee Ivaturi <dhanvee_ivaturi@symantec.com>
2019-08-15 15:32:42 -0700

    add xkcd api support and json unmarshalling

--- main.go
+++ main.go
@@ -1,7 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/francoispqt/gojay"
+)
+
+// ===== [ MAIN ] ===============
 
 func main() {
-	fmt.Println("Hello World!")
+	getLatest().dump()
+}
+
+// ===== [ XKCD ] ===============
+
+// Comic is a struct that stores all information relevant to a comic
+type Comic struct {
+	Day       string
+	Month     string
+	Year      string
+	Num       int
+	Title     string
+	SafeTitle string
+	Link      string
+	Alt       string
+	Img       string
+}
+
+func (c *Comic) NKeys() int {
+	return 9
+}
+
+func (c *Comic) UnmarshalJSONObject(dec *gojay.Decoder, key string) error {
+	switch key {
+	case "day":
+		return dec.String(&c.Day)
+	case "month":
+		return dec.String(&c.Month)
+	case "year":
+		return dec.String(&c.Year)
+	case "num":
+		return dec.Int(&c.Num)
+	case "title":
+		return dec.String(&c.Title)
+	case "safe_title":
+		return dec.String(&c.SafeTitle)
+	case "link":
+		return dec.String(&c.Link)
+	case "alt":
+		return dec.String(&c.Alt)
+	case "img":
+		return dec.String(&c.Img)
+	}
+	return nil
+}
+
+// ===== [  ] ==========
+
+func get(apiURL string) (c Comic) {
+	// Get data from XKCD api endpoint
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	// Read response body into a []byte
+	body, err := ioutil.ReadAll(resp.Body)
+
+	// Create a comic struct and populate it with xkcd data
+	c = Comic{}
+	err = gojay.UnmarshalJSONObject(body, &c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c
+}
+
+func getLatest() (c Comic) {
+	return get("https://xkcd.com/info.0.json")
+}
+
+// ===== [  ] ==========
+
+func (c Comic) dump() {
+	fmt.Println(c)
 }
